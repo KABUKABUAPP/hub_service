dd7eae3f0a64556132e2b6699b46ec5687192dff
luqman-shof <luqmanshof@gmail.com>
2025-11-02 10:01:40 +0100

    health check endpoint added

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const hubRoute = require("../modules/hub/route");
 const otpRoute = require("../modules/otp/route");
 const adminHubRoutes = require('../modules/admin/hub/route')
 const adminInspectorRoutes = require('../modules/admin/inspector/route')
+const healthRoute = require('./health')
 
 
 module.exports = (app) => {
@@ -11,6 +12,7 @@ module.exports = (app) => {
   app.use("/inspector", inspectorRoute);
   app.use("/hub", hubRoute);
   app.use("/otp", otpRoute);
+  app.use(healthRoute);
 
 
 
